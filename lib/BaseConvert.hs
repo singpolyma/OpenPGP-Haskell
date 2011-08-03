bde6687c6016bc81a95dbc033e475fce8a0e2594
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-08-02 19:06:25 -0500

    BaseConvert refactor again

--- lib/BaseConvert.hs
+++ lib/BaseConvert.hs
@@ -1,15 +1,15 @@
 module BaseConvert (toString, toAlphaDigit) where
 
-import Data.List
+import Data.Sequence
+import Data.Foldable
 
 toBase :: (Integral a) => a -> a -> [a]
 toBase _ 0 = [0]
-toBase b v = reverse $
-	unfoldr (\n -> if n == 0 then Nothing else Just (m n)) v
-	where m n = let (q, r) = n `divMod` b in (r, q)
+toBase b v = toList $
+	unfoldl (\n -> if n == 0 then Nothing else Just (n `divMod` b)) v
 
 toAlphaDigit :: (Integral a) => a -> Char
-toAlphaDigit n = (['0'..'9'] ++ ['A'..]) !! fromIntegral n
+toAlphaDigit = ((!!) (['0'..'9'] ++ ['A'..])) . fromIntegral
 
 toString :: (Integral a) => a -> a -> String
 toString b v = map toAlphaDigit (toBase b v)
