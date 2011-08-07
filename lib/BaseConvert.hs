a6a2db8e24c90ca19abd8cd83ab6ef1d5e290f5c
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-08-07 16:38:04 -0500

    Add toNum

--- lib/BaseConvert.hs
+++ lib/BaseConvert.hs
@@ -1,7 +1,11 @@
-module BaseConvert (toString, toAlphaDigit) where
+module BaseConvert (toString, toNum, toAlphaDigit, fromAlphaDigit) where
 
 import Data.Sequence
-import Data.Foldable
+import Data.Foldable (toList)
+import Data.List
+import Data.Char
+
+digit_alphabet = ['0'..'9'] ++ ['A'..]
 
 toBase :: (Integral a) => a -> a -> [a]
 toBase _ 0 = [0]
@@ -9,7 +13,17 @@ toBase b v = toList $
 	unfoldl (\n -> if n == 0 then Nothing else Just (n `divMod` b)) v
 
 toAlphaDigit :: (Integral a) => a -> Char
-toAlphaDigit = ((!!) (['0'..'9'] ++ ['A'..])) . fromIntegral
+toAlphaDigit = ((!!) digit_alphabet) . fromIntegral
 
 toString :: (Integral a) => a -> a -> String
 toString b v = map toAlphaDigit (toBase b v)
+
+fromAlphaDigit :: (Num a) => Char -> a
+fromAlphaDigit v = fromIntegral n
+	where Just n = elemIndex (toUpper v) digit_alphabet
+
+fromBase :: (Num a) => a -> [a] -> a
+fromBase b v = foldl (\n k -> n * b + k) 0 v
+
+toNum :: (Num a) => a -> String -> a
+toNum b v = fromBase b (map fromAlphaDigit v)
